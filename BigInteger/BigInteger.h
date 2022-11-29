5dab1ea6045acbe1f3dac050717026de822d6c4b
c5xheavy <amir-baturov@mail.ru>
2022-11-29 18:29:43 +0300

    Changed tabulation

--- BigInteger/BigInteger.h
+++ BigInteger/BigInteger.h
@@ -6,29 +6,29 @@
 class BigInteger {
 public:
 
-	BigInteger();
-	BigInteger(const std::string&);
-	BigInteger(const BigInteger&);
+    BigInteger();
+    BigInteger(const std::string&);
+    BigInteger(const BigInteger&);
 	
-	friend std::istream& operator>>(std::istream&, BigInteger&);
-	friend std::ostream& operator<<(std::ostream&, const BigInteger&);
+    friend std::istream& operator>>(std::istream&, BigInteger&);
+    friend std::ostream& operator<<(std::ostream&, const BigInteger&);
 
-	const BigInteger operator-() const;
+    const BigInteger operator-() const;
+	
+    friend bool operator<(const BigInteger&, const BigInteger&);
 
-	friend bool operator<(const BigInteger&, const BigInteger&);
-
-	BigInteger& operator+=(const BigInteger&);
+    BigInteger& operator+=(const BigInteger&);
     const BigInteger operator+(const BigInteger&) const;
     BigInteger& operator-=(const BigInteger&);
     const BigInteger operator-(const BigInteger&) const;
 
 private:
 
-	static const int BASE = 10000;
-	static const int LEN = 4;
+    static const int BASE = 10000;
+    static const int LEN = 4;
 
-	std::vector<int> data;
-	bool minus = false;
+    std::vector<int> data;
+    bool minus = false;
 
-	void trim();
+    void trim();
 };
